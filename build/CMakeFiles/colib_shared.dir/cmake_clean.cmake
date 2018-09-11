601cf59909be8186adcf6bdfd441bfa803669ec0
alexichen <alexichen@tencent.com>
2018-09-11 20:54:02 +0800

    add libco src

---
+++ build/CMakeFiles/colib_shared.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/colib_shared.dir/co_epoll.cpp.o"
+  "CMakeFiles/colib_shared.dir/co_hook_sys_call.cpp.o"
+  "CMakeFiles/colib_shared.dir/co_routine.cpp.o"
+  "CMakeFiles/colib_shared.dir/coctx.cpp.o"
+  "CMakeFiles/colib_shared.dir/coctx_swap.S.o"
+  "libcolib.pdb"
+  "libcolib.0.5.dylib"
+  "libcolib.dylib"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang ASM CXX)
+  include(CMakeFiles/colib_shared.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/colib_static.dir/DependInfo.cmake
@@ -0,0 +1,32 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "ASM"
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_ASM
+  "/Users/alexichen/UnixSpace/libco/coctx_swap.S" "/Users/alexichen/UnixSpace/libco/build/CMakeFiles/colib_static.dir/coctx_swap.S.o"
+  )
+set(CMAKE_ASM_COMPILER_ID "Clang")
+
+# The include file search paths:
+set(CMAKE_ASM_TARGET_INCLUDE_PATH
+  )
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/alexichen/UnixSpace/libco/co_epoll.cpp" "/Users/alexichen/UnixSpace/libco/build/CMakeFiles/colib_static.dir/co_epoll.cpp.o"
+  "/Users/alexichen/UnixSpace/libco/co_hook_sys_call.cpp" "/Users/alexichen/UnixSpace/libco/build/CMakeFiles/colib_static.dir/co_hook_sys_call.cpp.o"
+  "/Users/alexichen/UnixSpace/libco/co_routine.cpp" "/Users/alexichen/UnixSpace/libco/build/CMakeFiles/colib_static.dir/co_routine.cpp.o"
+  "/Users/alexichen/UnixSpace/libco/coctx.cpp" "/Users/alexichen/UnixSpace/libco/build/CMakeFiles/colib_static.dir/coctx.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "Clang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
